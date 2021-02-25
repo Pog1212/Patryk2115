c7d6fc9a48439cd605a5a1a8c3824efeb9f13fa8
Pog1212 <79515008+Pog1212@users.noreply.github.com>
2021-02-25 13:40:47 +0100

    Add files via upload

---
+++ zadanka.java
@@ -0,0 +1,27 @@
+package test;
+
+public class zadanka {
+    public static void main(String[] args) {
+        // zadanie 2
+        int A = 2;
+        int B = 3;
+        int sum = (A * A) + (B * B);
+        System.out.println("wynik = " + sum);
+
+        // zadanie 1
+        int intA = 5;
+        double doubleA = 3.4 ;
+        char CharA = '!';
+        String StringA = " jest wieksze od ";
+        System.out.println(intA + StringA + doubleA + CharA);
+
+        // zadanie 4
+        String myStringA = "Patryk";
+        String myStringB = "Likszo";
+
+        System.out.println(myStringA + "\n" + myStringB);
+
+
+    }
+}
+
